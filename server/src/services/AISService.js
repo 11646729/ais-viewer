24812f66856ac843a21ab45fa8109d8e64c7c8ed
11646729 <bds6052@gmail.com>
2025-11-20 09:47:20 +0000

    Minor edit

--- server/src/services/AISService.js
+++ server/src/services/AISService.js
@@ -19,7 +19,7 @@ function connectToAISStream(onMessage) {
           [90, 180],
         ],
       ],
-      // FilterMessageTypes: ["PositionReport"],
+      FilterMessageTypes: ["PositionReport"],
       FiltersShipMMSI: [], // Empty array means no filtering; receive all vessels
       // FiltersShipMMSI: ["311000807", "310812000", "205011000", "477886700"], // Virgin Cruises - Scarlet Lady & Discovery Princess
     }
