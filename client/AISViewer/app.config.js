88f137deeced2887a721d5cbcbc002f83c969679
11646729 <bds6052@gmail.com>
2025-11-27 09:19:59 +0000

    Added expo & ios files

--- client/AISViewer/app.config.js
+++ client/AISViewer/app.config.js
@@ -1,51 +1,52 @@
-import "dotenv/config";
+import "dotenv/config"
 
 export default {
-  "expo": {
-    "name": "AISViewer",
-    "slug": "AISViewer",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/images/icon.png",
-    "scheme": "aisviewer",
-    "userInterfaceStyle": "automatic",
-    "newArchEnabled": true,
-    "ios": {
-      "supportsTablet": true
+  expo: {
+    name: "AISViewer",
+    slug: "AISViewer",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "aisviewer",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.anonymous.AISViewer",
     },
-    "android": {
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/images/adaptive-icon.png",
-        "backgroundColor": "#ffffff"
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
       },
-      "edgeToEdgeEnabled": true,
-      "package": "com.anonymous.AISViewer"
+      edgeToEdgeEnabled: true,
+      package: "com.anonymous.AISViewer",
     },
-    "web": {
-      "bundler": "metro",
-      "output": "static",
-      "favicon": "./assets/images/favicon.png"
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
     },
-    "plugins": [
+    plugins: [
       "expo-router",
       [
         "expo-splash-screen",
         {
-          "image": "./assets/images/splash-icon.png",
-          "imageWidth": 200,
-          "resizeMode": "contain",
-          "backgroundColor": "#ffffff"
-        }
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
       ],
       [
         "@rnmapbox/maps",
         {
-          "RNMapboxMapsDownloadToken": process.env.MAPBOX_DOWNLOAD_TOKEN
-        }
-      ]
+          RNMapboxMapsDownloadToken: process.env.MAPBOX_DOWNLOAD_TOKEN,
+        },
+      ],
     ],
-    "experiments": {
-      "typedRoutes": true
-    }
-  }
+    experiments: {
+      typedRoutes: true,
+    },
+  },
 }
